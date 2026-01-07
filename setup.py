2da11fb65dbaa2667283ef22204921394ef99872
Yanjie Dong <yanjie.dong@hotmail.com>
2026-01-07 15:48:43 +0000

    updated python version and added missing file

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.12, <3.13',
 )
